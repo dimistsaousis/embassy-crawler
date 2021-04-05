85cdd61b69c320ee8f3c5af4eb20db9fb36292dd
Dimis Tsaousis <dimis.tsaousis@oaknorth.ai>
2021-04-05 19:44:17 +0100

    second commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import time
 import subprocess
 
-ADDRESS = 'https://www.greekembassy.org.uk/en-gb/Reservations/ctl/ViewAvailability/mid/484/List/ViewAvailabilityListSettings'
+ADDRESS = "https://www.greekembassy.org.uk/en-gb/Reservations/ctl/ViewAvailability/mid/484/List/ViewAvailabilityListSettings"
 
 
 def read_tables(address):
@@ -14,34 +14,34 @@ def get_appointments_table(address):
     table = dfs[0]
     table.columns = table.iloc[1, :]
     table = table.iloc[2:, 1:]
-    table = table.set_index('Category')
+    table = table.set_index("Category")
     return table
 
 
 def get_military(address):
-    index = '3. Military Affairs / Permanent Residence Certificates - Στρατολογικά'
+    index = "3. Military Affairs / Permanent Residence Certificates - Στρατολογικά"
     table = get_appointments_table(address)
     return table.loc[index]
 
 
 def has_availability(military):
-    availability = military['Availability']
-    return False if availability == '0' else True
+    availability = military["Availability"]
+    return False if availability == "0" else True
 
 
 def next_available_date(military):
-    return military['Next Available Date']
+    return military["Next Available Date"]
 
 
 def monitor():
     military = get_military(ADDRESS)
     count = 0
     while not has_availability(military):
-        print(f'({count}): No appointments available')
+        print(f"({count}): No appointments available")
         count += 1
         time.sleep(10)
         military = get_military(ADDRESS)
-    print(f'Found an appointment, elapsed time: {count*10} seconds')
+    print(f"Found an appointment, elapsed time: {count*10} seconds")
     alarm()
 
 
@@ -51,5 +51,5 @@ def alarm():
         subprocess.call(["afplay", audio_file])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     monitor()
