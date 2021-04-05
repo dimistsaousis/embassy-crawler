0119763a287d1b13e55749623164ee5e5767ef95
Dimis Tsaousis <dimis.tsaousis@oaknorth.ai>
2021-04-05 19:45:03 +0100

    third commit

--- .venv/lib/python3.8/site-packages/openal/_al.py
+++
@@ -1,5 +0,0 @@
-from al import *
-
-import warnings
-
-warnings.warn(DeprecationWarning("This submodule will no longer be supported in a future release.\nPlease use 'al' instead"))
--- .venv/lib/python3.8/site-packages/openal/_alc.py
+++
@@ -1,5 +0,0 @@
-from alc import *
-
-import warnings
-
-warnings.warn(DeprecationWarning("This submodule will no longer be supported in a future release.\nPlease use 'alc' instead"))
